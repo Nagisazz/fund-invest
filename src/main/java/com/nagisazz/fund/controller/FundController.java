376d9b5d6972d584550419cf6e6b232bff9cc53e
NagisaZZ <597306107@qq.com>
2022-09-08 18:53:31 +0800

    fix

--- src/main/java/com/nagisazz/fund/controller/FundController.java
+++ src/main/java/com/nagisazz/fund/controller/FundController.java
@@ -1,16 +1,14 @@
 package com.nagisazz.fund.controller;
 
+import com.nagisazz.fund.service.FundService;
+import com.nagisazz.fund.vo.ParamData;
+import com.nagisazz.fund.vo.ResultData;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.nagisazz.fund.service.FundService;
-import com.nagisazz.fund.vo.ParamData;
-import com.nagisazz.fund.vo.ResultData;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
@@ -21,7 +19,8 @@ public class FundController {
 
     @GetMapping("/cal")
     public ResultData cal(@RequestParam("data") String data) {
-        String[] params = data.split("#");
+        log.info("参数：{}", data);
+        String[] params = data.split("\\$");
         ParamData paramData = ParamData.builder().code(params[0]).date(params[1]).frequence(params[2]).invest(params[3]).balance(params[4])
                 .worth(params[5]).yields(params[6]).mean(params[7]).wave(params[8]).type(params[9]).build();
         if (StringUtils.isBlank(fundService.crawler(paramData.getCode()))) {
